93fa06d34aef9ea1307d4473f88ca07125dc3e0c
Gabriel Cardona <gjcardonam@gmail.com>
2025-04-13 15:08:32 -0500

    fix cors production methods

--- src/main/java/com/udea/bancoudea/cors/WebConfig.java
+++ src/main/java/com/udea/bancoudea/cors/WebConfig.java
@@ -1,6 +1,5 @@
 package com.udea.bancoudea.cors;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
@@ -9,9 +8,10 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // todas las rutas
-                .allowedOrigins("https://bancoudea-frontend.onrender.com") // tu frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+        registry.addMapping("/**")
+                .allowedOrigins("https://bancoudea-frontend.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
     }
 }
